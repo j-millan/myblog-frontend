efe8bf2f83d78675ce5e43dc03bbbf709fec0288
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-15 19:08:28 -0400

    create api service

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  API_URL = 'https://127.0.0.1:800/'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  post<RT>(path: string, data: any = {}): Observable<RT> {
+    const url = `${this.API_URL}${path}`
+    return this.http.post<RT>(url, data);
+  }
+
+  get<RT>(path: string): Observable<RT> {
+    const url = `${this.API_URL}${path}`
+    return this.http.get<RT>(url);
+  }
+
+  put<RT>(path: string, data: any = {}): Observable<RT> {
+    const url = `${this.API_URL}${path}`;
+    return this.http.put<RT>(url, data);
+  }
+
+  delete(path: string, data: any = {}): Observable<void> {
+    const url = `${this.API_URL}${path}`;
+    return this.http.request<void>('delete', url, { body: data });
+  }
+}
