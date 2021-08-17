5bb393ddaeaa4303642f134365c8c06d686197a6
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-17 08:39:42 -0400

    generate login component and route

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
-
+  { 
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forChild(routes),
   ],
   exports: [RouterModule],
 })
