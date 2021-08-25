5bd323a6e7e068d833543ddacfd3b883e1abe07f
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-25 17:14:07 -0400

    finish carousel component

--- src/app/modules/blog/components/blog-post-carousel/blog-post-carousel.component.ts
+++ src/app/modules/blog/components/blog-post-carousel/blog-post-carousel.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BaseComponent } from 'src/app/base.component';
+import { BlogPost } from 'src/app/data/models/blog-post';
 
 @Component({
   selector: 'app-blog-post-carousel',
   templateUrl: './blog-post-carousel.component.html',
   styleUrls: ['./blog-post-carousel.component.scss']
 })
-export class BlogPostCarouselComponent implements OnInit {
+export class BlogPostCarouselComponent extends BaseComponent {
+  @Input() blogPosts: BlogPost[];
+  currentIndex: number = 0;
+  get currentPost(): BlogPost {
+    return this.blogPosts[this.currentIndex];
+  };
 
-  constructor() { }
+  nextPost(): void {
+    this.currentIndex = Math.max(
+      0, 
+      Math.min(this.currentIndex + 1, this.blogPosts.length - 1)
+    );
+    console.debug(this.currentIndex)
+  }
 
-  ngOnInit(): void {
+  previousPost(): void {
+    this.currentIndex = Math.max(
+      0,
+      Math.min(this.currentIndex - 1, this.blogPosts.length - 1)
+    );
+    console.debug(this.currentIndex)
   }
 
 }
--- src/app/modules/blog/pages/home/home.component.ts
+++ src/app/modules/blog/pages/home/home.component.ts
@@ -10,12 +10,12 @@ import { BlogPostService } from 'src/app/data/services/blog-post.service';
 })
 export class HomeComponent implements OnInit {
 
-  blogPost$: Observable<BlogPost>;
+  blogPosts$: Observable<BlogPost[]>;
 
   constructor(private blogPostService: BlogPostService) { }
   
   ngOnInit(): void {
-    this.blogPost$ = this.blogPostService.getBlogPost(11);
+    this.blogPosts$ = this.blogPostService.getBlogPosts();
   }
 
 }
