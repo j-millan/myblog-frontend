467a302462bd515a427077e76afffe2425bf3828
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-24 22:14:55 -0400

    generate blog post service

---
+++ src/app/data/services/blog-post.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api.service';
+import { BlogPost } from '../models/blog-post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  readonly BLOG_POST_LIST_CREATE_PATH = '/blog-posts';
+  readonly BLOG_POST_DETAIL_UPDATE_DELETE_PATH = '/blog-posts/blogPostId';
+  
+  constructor(private api: ApiService) { }
+
+  getBlogPost(blogPostId: number): Observable<BlogPost> {
+    const path = this.api.replaceParams(
+      this.BLOG_POST_DETAIL_UPDATE_DELETE_PATH,
+      { blogPostId: blogPostId },
+    );
+
+    return this.api.get<BlogPost>(path);
+  }
+}
