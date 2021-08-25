cf481cfca6c3066f92d68bb2b33047e057c97683
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-25 17:27:05 -0400

    truncate blog post introductions

--- src/app/modules/blog/components/blog-post-card/blog-post-card.component.ts
+++ src/app/modules/blog/components/blog-post-card/blog-post-card.component.ts
@@ -6,7 +6,7 @@ import { BlogPost } from 'src/app/data/models/blog-post';
 @Component({
   selector: 'app-blog-post-card',
   templateUrl: './blog-post-card.component.html',
-  styleUrls: ['./blog-post-card.component.scss']
+  styleUrls: ['./blog-post-card.component.scss'],
 })
 export class BlogPostCardComponent extends BaseComponent {
   @Input() blogPost: BlogPost;
