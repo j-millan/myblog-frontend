e465f47201d11a253b35c06689bb952c93a87927
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-24 20:03:49 -0400

    import blog routing module into blog module

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BlogRoutingModule } from './blog-routing.module';
 
 
 
@@ -11,6 +12,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     ReactiveFormsModule,
+    BlogRoutingModule,
     SharedModule,
   ]
 })
