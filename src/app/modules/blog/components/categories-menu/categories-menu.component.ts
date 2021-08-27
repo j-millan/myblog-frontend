a68ee098874bd471681a9a8b59eba57bfccf029c
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-27 14:46:53 -0400

    add "More" option to cateory menu

--- src/app/modules/blog/components/categories-menu/categories-menu.component.ts
+++ src/app/modules/blog/components/categories-menu/categories-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { BlogCategoryService } from 'src/app/data/services/blog-category.service';
 
 @Component({
@@ -7,7 +8,11 @@ import { BlogCategoryService } from 'src/app/data/services/blog-category.service
   styleUrls: ['./categories-menu.component.scss']
 })
 export class CategoriesMenuComponent {
-  categories$ = this.blogCategoryService.getBlogCategories();
+  categories$ = this.blogCategoryService
+    .getBlogCategories()
+    .pipe(
+      map((categories) => categories.slice(0, 7)),
+    );
   
   constructor(
     private blogCategoryService: BlogCategoryService
