0a98d3a1c0ed1731d80e0667818d34d51e8509b5
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-27 13:28:20 -0400

    create service to get most popular blog categories

--- src/app/data/services/blog-category.service.ts
+++ src/app/data/services/blog-category.service.ts
@@ -9,10 +9,15 @@ import { BlogCategory } from '../models/blog-category';
 export class BlogCategoryService {
 
   readonly BLOG_CATEGORY_LIST_PATH = 'blog-categories';
+  readonly POPULAR_BLOG_CATEGORY_LIST_PATH = 'blog-categories/popular';
 
   constructor(private api: ApiService) { }
 
   getBlogCategories(): Observable<BlogCategory[]> {
     return this.api.get<BlogCategory[]>(this.BLOG_CATEGORY_LIST_PATH);
+  }
+
+  getPopularBlogCategories(): Observable<BlogCategory[]> {
+    return this.api.get<BlogCategory[]>(this.POPULAR_BLOG_CATEGORY_LIST_PATH);
   }
 }
