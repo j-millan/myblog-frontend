57b9f5bf068da3295db58af48ab068bcddbe8cc1
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-27 13:28:50 -0400

    add filter to getBlogPosts service

--- src/app/data/services/blog-post.service.ts
+++ src/app/data/services/blog-post.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
 import { BlogPost } from '../models/blog-post';
+import { BlogPostFilter } from '../models/filters/blog-post.filter';
+import { SerializerService } from './serializer.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,9 @@ export class BlogPostService {
   readonly BLOG_POST_LIST_CREATE_PATH = 'blog-posts';
   readonly BLOG_POST_DETAIL_UPDATE_DELETE_PATH = 'blog-posts/blogPostId';
   
-  constructor(private api: ApiService) { }
+  constructor(
+    private api: ApiService,
+  ) { }
 
   getBlogPost(blogPostId: number): Observable<BlogPost> {
     const path = this.api.replaceParams(
@@ -22,7 +26,10 @@ export class BlogPostService {
     return this.api.get<BlogPost>(path);
   }
 
-  getBlogPosts(): Observable<BlogPost[]> {
-    return this.api.get<BlogPost[]>(this.BLOG_POST_LIST_CREATE_PATH);
+  getBlogPosts(filter?: BlogPostFilter): Observable<BlogPost[]> {
+    let queryString = this.api.getQuerystring(filter);
+    return this.api.get<BlogPost[]>(
+      this.BLOG_POST_LIST_CREATE_PATH + queryString,
+    );
   }
 }
